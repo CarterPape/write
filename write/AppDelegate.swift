a166bb79a9871a2a994974e802252aa4bf52024e
Carter Pape <carterpape@Carters-MacBook-Pro.local>
2018-06-22 18:29:43 -0400

    Initial Commit

---
+++ write/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  write
+//
+//  Created by Carter Pape on 6/22/18.
+//  Copyright © 2018 Carter Pape. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ write/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  write
+//
+//  Created by Carter Pape on 6/22/18.
+//  Copyright © 2018 Carter Pape. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
